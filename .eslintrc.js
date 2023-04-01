58c8df6075af4a24d11c79567d75de57daf7d183
Emi Acerbi <96954868+emiacerbi@users.noreply.github.com>
2023-04-01 13:52:48 -0300

    Dev (#5)

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: [
+    'plugin:tailwindcss/recommended',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'next/core-web-vitals',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  root: true,
+};
